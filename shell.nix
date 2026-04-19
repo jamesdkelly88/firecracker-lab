2829fa70e648665c89f0960ce477af7f2f61f7fb
James Kelly <jamesdkelly88@outlook.com>
2026-04-19 17:04:44 +0100

    attempt 2 - still not working

--- shell.nix
+++ shell.nix
@@ -1,6 +1,8 @@
 { pkgs ? import <nixpkgs> {config.allowUnfree = true;} }:pkgs.mkShell {
   packages = with pkgs; [
     firecracker
+    gh
+    gnumake
     go-task
     linux-scripts
     squashfsTools
